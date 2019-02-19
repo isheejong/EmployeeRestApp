fbae414ca2cd188b406aa1f90abf862de8b557b8
Dong Zhu <bluezhudong@gmail.com>
2019-02-19 15:10:06 +0800

    APIApplication.java: remove the annotation
    Signed-off-by: Dong Zhu <bluezhudong@gmail.com>

--- src/main/java/com/developer/service/APIApplication.java
+++ src/main/java/com/developer/service/APIApplication.java
@@ -8,13 +8,11 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class APIApplication extends ResourceConfig {
 	
 	public APIApplication() {
-        //加载Resource
+
         register(EmployeeController.class);
 
-        //注册数据转换器
         register(JacksonJsonProvider.class);
 
-        // Logging.
         register(LoggingFilter.class);
     }
 
